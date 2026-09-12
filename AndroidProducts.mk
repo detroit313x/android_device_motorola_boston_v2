ccea505563f277c01f35ffd95f017a0808b972e8
detroit313x <37162313+detroit313x@users.noreply.github.com>
2026-09-12 17:24:48 -0600

    boston v2: Add AndroidProducts.mk for Android 12.0 PBRP

---
+++ AndroidProducts.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2026 PitchBlack Recovery Project
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/pb_boston.mk
+
+COMMON_LUNCH_CHOICES := \
+    pb_boston-userdebug \
+    pb_boston-eng
