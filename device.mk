b9ca673aa2b8080fcdd2963ce062fa653d08dbcf
detroit313x <37162313+detroit313x@users.noreply.github.com>
2026-09-16 01:23:53 -0600

    boston: Add device.mk

--- device.mk
+++ device.mk
@@ -1,4 +1,21 @@
+#
+# Copyright (C) 2026 PitchBlack Recovery Project
+#
+
 LOCAL_PATH := device/motorola/boston
 
-PRODUCT_PACKAGES += android.hardware.fastboot@1.0-impl fastbootd qcom_decrypt
-PRODUCT_COPY_FILES += $(LOCAL_PATH)/recovery/root/init.recovery.qcom.rc:recovery/root/init.recovery.qcom.rc
+# Enable Fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl \
+    fastbootd
+
+# Dynamic Partitions & Decryption Storage Tools (Android 12+)
+PRODUCT_PACKAGES += \
+    qcom_decrypt \
+    qcom_decrypt_fsg \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
+
+# Include Ramdisk Recovery Init Configuration
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/recovery/root/init.recovery.qcom.rc:recovery/root/init.recovery.qcom.rc
